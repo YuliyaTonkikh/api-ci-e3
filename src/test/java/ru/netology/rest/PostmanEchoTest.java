11a445234ec578c78f0ef371187b9b9b5a6fd7ae
Yuliya Tonkikh <github@amnesia-air.local>
2023-03-16 09:59:08 +0700

    fix based on comments

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -13,95 +13,22 @@ import static org.hamcrest.Matchers.*;
 class PostmanEchoTest {
 
     private RequestSpecification specs = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
-            .setBasePath("/api/v1")
-            .setPort(9999)
-            .setAccept(ContentType.JSON)
-            .setContentType(ContentType.JSON)
+            .setBaseUri("https://postman-echo.com")
             .log(LogDetail.ALL)
             .build();
 
     @Test
-    @DisplayName("should currency RUB")
-    void shouldCurrencyRub() {
+    @DisplayName("Test postman echo")
+    void testPostman() {
         given()
                 .spec(specs)
+                .body("Hell world")
                 .when()
-                .get("/demo/accounts")
-                .then()
-                .body("[0].currency", equalTo("RUB"));
-
-    }
-
-    @Test
-    @DisplayName("Should currency USD")
-    void shouldCurrencyUsd() {
-        given()
-                .spec(specs)
-                .when()
-                .get("/demo/accounts")
-                .then()
-                .body("[1].currency", equalTo("USD"));
-    }
-
-    @Test
-    @DisplayName("Should currency RUB and USD")
-    void shouldCurrencyRubUsd() {
-        given()
-                .spec(specs)
-                .when()
-                .get("/demo/accounts")
-                .then()
-                .contentType(ContentType.JSON)
-                .body("", hasSize(3))
-                .body("[2].currency", equalTo("RUB"))
-                .body("[1].currency", equalTo("USD"))
-                .body("[0].balance", greaterThanOrEqualTo(0));
-
-    }
-
-    @Test
-    @DisplayName("Should return JSON specs")
-    void shouldReturnJSONResponseSpecs() {
-        // Given - When - Then
-        // Предусловия
-        given()
-                .spec(specs)
-                .when()
-                .get("/demo/accounts")
+                .post("/post")
                 .then()
                 .statusCode(200)
-                .header("Content-Type", "application/json; charset=UTF-8")
-                .contentType(ContentType.JSON)
-        ;
-    }
-
-    @Test
-    @DisplayName("Check what first account in list have balance greater or equal zero")
-    void firstAccountInListHaveBalanceGreaterOrEqualZero() {
-        given().spec(specs)
-                .when()
-                .get("/demo/accounts")
-                .then()
-                .statusCode(200)
-                .contentType(ContentType.JSON)
-                .body("", hasSize(3))
-                .body("[0].currency", equalTo("RUB"))
-                .body("[0].balance", greaterThanOrEqualTo(0))
-        ;
-    }
-
-    @Test
-    @DisplayName("Check all balance of accounts is greater or equal zero")
-    void allBalanceGreaterOrEqualZero() {
-        given()
-                .spec(specs)
-                .when()
-                .get("/demo/accounts")
-                .then()
-                .statusCode(200)
-                .contentType(ContentType.JSON)
-                .body("every{ it.balance >= 0 }", is(true));
+                .body("data", equalTo("Hell world"));
 
     }
+
 }
